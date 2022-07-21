8dbce852fc46c88ccfb9f228b9541e82c69ba875
ahmed-farid <ahmed.farid0002@gmail.com>
2022-07-21 11:11:30 +0200

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,94 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+
+module.exports = {
+
+    entry: './src/js/index.js',
+
+    output: {
+        // publicPath: '/',
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/main.js',
+    },
+
+    module: {
+        rules: [
+          {
+            test: /\.(sass|css|scss)$/,
+            use: [
+              // Creates `style` nodes from JS strings
+              MiniCssExtractPlugin.loader,
+              // {
+              //   loader: MiniCssExtractPlugin.loader,
+              //   options: {
+              //     publicPath: '../'
+              //   }
+              // },
+              // Translates CSS into CommonJS
+              "css-loader",
+              // Compiles Sass to CSS
+              "sass-loader",
+            ],
+          },
+          {
+            test: /\.(png|jpe?g|gif)$/i,
+            use: [
+              {
+                loader: 'file-loader',
+                options: {
+                name: '[name].[ext]',
+                outputPath: "images",
+                },
+              },
+            ],
+          },
+          {
+            test: /\.(svg|woff|woff2|ttf|eot)$/,
+            use: [
+              {
+                loader: 'file-loader',
+                options: {
+                name: '[name].[ext]',
+                outputPath: "fonts", 
+                },
+              },
+            ],
+          },
+          {
+            test: /\.html$/i,
+            loader: "html-loader",
+          },
+          {
+            test: require.resolve("jquery"),
+            loader: "expose-loader",
+            options: {
+              exposes: ["$", "jQuery"],
+            },
+          },
+        ],
+    },
+
+    devServer: {
+      static: {
+        directory: path.join(__dirname, "dist")
+      },
+        // compress: true,
+        port: 1238,
+        devMiddleware: {
+          writeToDisk: true,
+        },
+        // stats: 'errors-only',
+        open: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+        template: './src/index.html',
+        filename: 'index.html',
+        }),
+
+        new MiniCssExtractPlugin({
+            filename: 'css/style.css',
+        }),
+    ],
+};
